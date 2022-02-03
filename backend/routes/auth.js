d5daaebceff07043d6fafff3bbf1efdad32d1e4b
Hemant <hemantrastogi17@gmail.com>
2022-02-03 15:00:02 +0530

    Fetch user data route and middleware created

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,9 +4,9 @@ const router = express.Router()
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
+const fetchuser = require('../middleware/fetchuser');
 const JWT_SECRET = "HemantakaFirefist"
-// Create a user using POST "api/auth/createuser". Doesn't require authentication
+//ROUTE 1 : Create a user using POST "api/auth/createuser". Doesn't require authentication
 router.post('/createuser', [
     body('name', "Enter a valid name").isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
@@ -45,7 +45,7 @@ router.post('/createuser', [
         res.status(500).send("Internal Server Error")
     }
 })
-// Authenticate  a user using POST "api/auth/login". No login required.
+// ROUTE2 : Authenticate  a user using POST "api/auth/login". No login required.
 router.post('/login', [
     body('email', 'Enter a valid email').isEmail(),
     body('password', "Password cannot be blank").exists(),
@@ -72,6 +72,23 @@ router.post('/login', [
         const authtoken = jwt.sign(data, JWT_SECRET)
         console.log(data);
         res.json({ authtoken })
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Internal Server Error")
+    }
+})
+
+// ROUTE 3 : Get logged in user details using POST : "/api/auth/getuser". Login required
+router.post('/getuser',fetchuser, [
+    body('email', 'Enter a valid email').isEmail(),
+
+
+], async (req, res) => {
+
+    try {
+        userId = req.user.id
+        const user = await User.findById(userId).select("-password")
+        res.send(user)
     } catch (error) {
         console.error(error.message)
         res.status(500).send("Internal Server Error")
